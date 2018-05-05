030eb0557dde5076eeb701fb7248557c1f5af01d
Marcus <mm443615@ohio.edu>
2018-05-05 01:11:59 -0400

    Home Page looks good and runs

---
+++ JAMM/JAMM/HomePageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JAMM
+//
+//  Created by Marcus McKinley on 5/5/18.
+//  Copyright © 2018 Marcus McKinley. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func setupUI() {
+        let title = UILabel()
+        title.font = UIFont(name: "HelveticaNeue-Bold", size: 20.0)
+        title.text = "Fortnite Guide"
+        title.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        self.navigationItem.titleView = title
+    }
+
+}
+

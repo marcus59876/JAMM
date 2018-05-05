76aaa4a159f50cd242bfae0b4e7ae163cf7d043e
Marcus <mm443615@ohio.edu>
2018-05-05 02:48:02 -0400

    added a couple more screens

---
+++ JAMM/JAMM/Jump Roulette/JumpRouletteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  JumpRouletteViewController.swift
+//  JAMM
+//
+//  Created by Marcus McKinley on 5/5/18.
+//  Copyright © 2018 Marcus McKinley. All rights reserved.
+//
+
+import UIKit
+
+class JumpRouletteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    func setupUI() {
+        
+    }
+    
+
+}
---
+++ JAMM/JAMM/Weapon Compare/WeaponCompareViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WeaponCompareViewController.swift
+//  JAMM
+//
+//  Created by Marcus McKinley on 5/5/18.
+//  Copyright © 2018 Marcus McKinley. All rights reserved.
+//
+
+import UIKit
+
+class WeaponCompareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    func setupUI() {
+        
+    }
+
+}
